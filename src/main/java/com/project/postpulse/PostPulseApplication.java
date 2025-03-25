6d97dde9034a27de04a85e969e41c1dbb0e7a075
Jae-HyeokKim <jaehyeok.ethan@gmail.com>
2025-03-26 03:39:12 +0900

    Initial commit

---
+++ src/main/java/com/project/postpulse/PostPulseApplication.java
@@ -0,0 +1,13 @@
+package com.project.postpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/postpulse/PostPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.postpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
