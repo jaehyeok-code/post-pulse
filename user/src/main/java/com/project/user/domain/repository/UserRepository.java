03da6894555e5e3b2fc069a9b682c44515aa9900
Jae-HyeokKim <jaehyeok.ethan@gmail.com>
2025-03-30 22:49:53 +0900

    feat: 멀티 모듈 프로젝트 초기 설정 및 user 엔티티/리포지토리 추가

---
+++ user/src/main/java/com/project/user/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.project.user.domain.repository;
+
+import com.project.user.domain.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByEmail(String email);
+
+}
